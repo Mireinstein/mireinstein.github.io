8fb8b202a9320dbfeb388b7ebe33eabeee0a091d
amadyira25@amherst.edu <amadyira25@amherst.edu>
2025-05-29 15:26:42 -0400

    chore: deploy build to docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ─── Base URL ───────────────────────────────────────────────────────────────
   base: '/',
+
+  // ─── Build output ───────────────────────────────────────────────────────────
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,   // clean docs/ on each build
+  },
 })
